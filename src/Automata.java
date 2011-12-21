cd632de6e79014a9b95e431e6e20f43ced943b59
Jason Liu <r99921041@ntu.edu.tw>
2011-12-21 23:41:58 +0800

    Updated Automata interface definition and implementation.

--- src/Automata.java
+++ src/Automata.java
@@ -3,105 +3,87 @@
  * and open the template in the editor.
  */
 
-import java.awt.geom.Point2D;
 import java.util.*;
 
 /**
  *
  * @author wkren
  */
-public class Automata implements AutomataInterface{
-
-	/**
-	 * @return the geometryData
-	 */
-//	public Geometry getGeometryData() {
-//		return geometricData;
-//	}
-
-	/**
-	 * @param geometryData the geometryData to set
-	 */
-//	public void setGeometryData(Point2D x, Point2D y) {
-//		this.geometricData.x = x;
-//		this.geometricData.y = y;
-//	}
-
-	/**
-	 * @return the drawingData
-	 */
-//	public Drawing getDrawingData() {
-//		return drawingData;
-//	}
-
-	/**
-	 * @param drawingData the drawingData to set
-	 */
-//	public void setDrawingData(Drawing drawingData) {
-//		this.drawingData = drawingData;
-//	}
+public class Automata implements AutomataInterface {
 
 	/**
 	 * @return the Type
 	 */
-	public Type getType() {
-		return this.pmType;
+	@Override
+	public WritingData getWritingData() {
+		return this.pmWritingData;
 	}
 
-	public List<Object> getAlphabet() {
+	@Override
+	public void setWritingData(WritingData writingData) {
+		this.pmWritingData = writingData;
+	}
+
+	@Override
+	public boolean isTransducer() {
+		return this.pmIsTransducer;
+	}
+
+	@Override
+	public void setIsTransducer(boolean isTransducer) {
+		this.pmIsTransducer = isTransducer;
+	}
+
+	@Override
+	public Weight getWeight() {
+		return this.pmWeight;
+	}
+
+	@Override
+	public void setWeight(Weight weight) {
+		this.pmWeight = weight;
+	}
+
+	@Override
+	public Alphabet getAlphabet() {
 		return this.pmAlphabet;
 	}
 
-	public void setAlphabet(List<Object> alphabet) {
+	@Override
+	public void setAlphabet(Alphabet alphabet) {
 		this.pmAlphabet = alphabet;
 	}
 
-	public List<Object> getSecondAlphabet() {
+	@Override
+	public Alphabet getSecondAlphabet() {
 		return this.pmSecondAlphabet;
 	}
 
-	public void setSecondAlphabet(List<Object> alphabet) {
+	@Override
+	public void setSecondAlphabet(Alphabet alphabet) {
 		this.pmSecondAlphabet = alphabet;
 	}
 
 	/**
-	 * @param valueype the valueType to set
-	 */
-//	public void setValueType(Type valueType) {
-//		this.valueType = valueType;
-//	}
-
-	
-
-	/**
 	 * @return the states
 	 */
+	@Override
 	public List<State> getAllStates() {
 		return this.pmAllStates;
-	}
-
-	public State getState(int index) {
-		return this.pmAllStates.get(index);
 	}
 
 	/**
 	 * @param states the states to set
 	 */
+	@Override
 	public void setAllStates(List<State> allStates) {
 		this.pmAllStates = allStates;
-	}
-
-	public void addState(State s) {
-		this.pmAllStates.add(s);
-	}
-
-	public void removeState(State state) {
-		this.pmAllStates.remove(state);
 	}
 
 	/**
 	 * @return the transitions
 	 */
+	@Override
 	public List<Transition> getAllTransitions() {
 		return this.pmAllTransitions;
 	}
@@ -109,60 +91,37 @@ public class Automata implements AutomataInterface{
 	/**
 	 * @param transitions the transitions to set
 	 */
+	@Override
 	public void setAllTransitions(List<Transition> transitions) {
 		this.pmAllTransitions = transitions;
 	}
-
-	public void addTransition(Transition t) {
-		this.pmAllTransitions.add(t);
-	}
-
-	public void removeTransition(Transition transition) {
-		this.pmAllTransitions.remove(transition);
-	}
-
-//	public class Geometry {
-//
-//		Point2D x;
-//		Point2D y;
-//	}
-//	private Geometry geometricData;
-//	private Drawing drawingData;
-	private Type pmType;
-	private List<Object> pmAlphabet;
-	private List<Object> pmSecondAlphabet;
+	private WritingData pmWritingData;
+	private boolean pmIsTransducer;
+	private Weight pmWeight;
+	private Alphabet pmAlphabet;
+	private Alphabet pmSecondAlphabet;
 	private List<State> pmAllStates;
 	private List<Transition> pmAllTransitions;
 
 	public Automata() {
-//		geometricData = new Geometry();
-//		drawingData = new Drawing();
-		this.pmType = new Type();
+		this.pmWeight = new Weight();
 		this.pmAllStates = new ArrayList<State>();
 		this.pmAllTransitions = new ArrayList<Transition>();
 	}
 
-	public Automata(Type type) {
-		this.pmType = type;
+	public Automata(Weight weight) {
+		this.pmWeight = weight;
 		this.pmAllStates = new ArrayList<State>();
 		this.pmAllTransitions = new ArrayList<Transition>();
 	}
 
 	public static void main(String args[]) {
 		System.out.println("Staring from 'Automata Class'");
-		System.out.println("Creating Monoid");
-		Monoid monoid = new Monoid();
-		System.out.println("Creating Semiring");
-		Semiring semiring = new Semiring("1", "0",
-				Semiring.SemiringType.NUMERICAL,
-				Semiring.SemiringSet.B,
-				Semiring.SemiringOperation.CLASSICAL,
-				"1", null, null);
-		System.out.println("Creating Type");
-		Type type = new Type();
+		System.out.println("Creating Weight");
+		Weight weight = new Weight();
 		System.out.println("Creating empty Automata");
 		Automata automata = new Automata();
-		System.out.println("Creating Automata with TYPE");
-		Automata automataWithType = new Automata(type);
+		System.out.println("Creating Automata with Weight");
+		Automata automataWithWeight = new Automata(weight);
 	}
-}
+}  // End public class Automata implements AutomataInterface
--- src/AutomataInterface.java
+++ src/AutomataInterface.java
@@ -7,66 +7,102 @@ import java.util.List;
  */
 public interface AutomataInterface {
 
-	public enum WeightSemiring {
+	public class WritingData {
 
-		B_BOOLEAN, Z_INTEGER, Q_RATIONAL, R_REAL, F2_TWO_ELEMENT_FIELD,
-		ZMIN_MIN_TROPICAL, ZMAX_MAX_TROPICAL
-	}
+		public Character closePar;
+		public Character openPar;
+		public Character plusSym;
+		public Character spacesSym;
+		public Character starSym;
+		public Character timesSym;
+		public Character weightClosing;
+		public Character weightOpening;
+		public Character zeroSym;
+	}  // End public class WritingData
 
-	public enum AlphabetDataType {
+	public class Weight {
 
-		CHAR, INT
-	}
+		public enum Semiring {
 
-	public static class Type {
-
-		boolean isTransducer;
-		WeightSemiring weightSemiring;
-		AlphabetDataType alphabetDataType;
-		AlphabetDataType secondAlphabetDataType;
-
-		public Type() {
-			this.isTransducer = false;
-			this.weightSemiring = WeightSemiring.B_BOOLEAN;
-			this.alphabetDataType = AlphabetDataType.CHAR;
-			this.secondAlphabetDataType = null;
+			B_BOOLEAN, Z_INTEGER, Q_RATIONAL, R_REAL, F2_TWO_ELEMENT_FIELD,
+			ZMIN_MIN_TROPICAL, ZMAX_MAX_TROPICAL
 		}
+		public Semiring semiring;
+		public Character identitySymbol;
+		public Character zeroSymbol;
 
-		public Type(
-				boolean isTransducer,
-				WeightSemiring weightSemiring,
-				AlphabetDataType alphabetDataType,
-				AlphabetDataType secondAlphabetDataType) {
-			this.isTransducer = isTransducer;
-			this.weightSemiring = weightSemiring;
-			this.alphabetDataType = alphabetDataType;
-			this.secondAlphabetDataType = secondAlphabetDataType;
+		public Weight() {
+			this.semiring = Semiring.B_BOOLEAN;
+			this.identitySymbol = null;
+			this.zeroSymbol = new Character('0');
 		}
-	}  // End public static class Type
+	}  // End public class Weight
 
-	public Type getType();
+	public class Alphabet {
 
-	public List<Object> getAlphabet();
+		public enum SymbolType {
 
-	public void setAlphabet(List<Object> alphabet);
+			CHAR, INT
+		}  // End public static class Symbol
+		public SymbolType symbolType;
+		public List<Object> allSymbols;
+		public Character identitySymbol;
+	}  // End public class Alphabet
 
-	public List<Object> getSecondAlphabet();
+	public WritingData getWritingData();
 
-	public void setSecondAlphabet(List<Object> alphabet);
+	public void setWritingData(WritingData writingData);
+
+	public boolean isTransducer();
+
+	public void setIsTransducer(boolean isTransducer);
+
+	public Weight getWeight();
+
+	public void setWeight(Weight weight);
+
+	public Alphabet getAlphabet();
+
+	public void setAlphabet(Alphabet alphabet);
+
+	public Alphabet getSecondAlphabet();
+
+	public void setSecondAlphabet(Alphabet alphabet);
 
 	public List<State> getAllStates();
 
 	public void setAllStates(List<State> allStates);
 
-	public void addState(State state);
-
-	public void removeState(State state);
-
 	public List<Transition> getAllTransitions();
 
 	public void setAllTransitions(List<Transition> allTransitions);
 
-	public void addTransition(Transition transition);
+	/*
+	 *  This class is used only to communicate with TAFKitInterface
+	 */
+	public class Type {
 
-	public void removeTransition(Transition transition);
+		public boolean isTransducer;
+		public Weight.Semiring weightSemiring;
+		public Alphabet.SymbolType alphabetSymbolType;
+		public Alphabet.SymbolType secondAlphabetSymbolType;
+
+		public Type() {
+			this.isTransducer = false;
+			this.weightSemiring = Weight.Semiring.B_BOOLEAN;
+			this.alphabetSymbolType = Alphabet.SymbolType.CHAR;
+			this.secondAlphabetSymbolType = null;
+		}
+
+		public Type(
+				boolean isTransducer,
+				Weight.Semiring weightSemiring,
+				Alphabet.SymbolType alphabetDataType,
+				Alphabet.SymbolType secondAlphabetDataType) {
+			this.isTransducer = isTransducer;
+			this.weightSemiring = weightSemiring;
+			this.alphabetSymbolType = alphabetDataType;
+			this.secondAlphabetSymbolType = secondAlphabetDataType;
+		}
+	}  // End public static class Type
 }  // End public interface AutomataInterface
